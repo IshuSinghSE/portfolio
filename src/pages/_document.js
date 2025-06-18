fee3cae707f0a61b36d88eb9d7b086ccc13905d1
Ishu <ishu.111636@gmail.com>
2025-06-18 23:32:32 +0530

    Added google ads tag

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -39,6 +39,8 @@ export default class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
+          <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6593809337731836"
+     crossorigin="anonymous"></script>
         </body>
       </Html>
     );
